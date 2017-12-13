b8f8de594e385ebd4ca5ddd11e6bb9145bf2317a
AiroHass <airo.hass@khk.ee>
2017-12-13 09:11:13 +0200

    Kirjeldatud seostatud tingimuslause!

--- tingimuslaused.php
+++ tingimuslaused.php
@@ -5,13 +5,30 @@
  * Date: 13.12.2017
  * Time: 8:44
  */
-$ilm = "hea";
-if($ilm=="sajab") {
-    echo "Väljas sajab, tuju on halb.";
+//$ilm = "sajab";
+//if($ilm=="sajab") {
+//    echo "Väljas sajab, tuju on halb.";
+//}
+//else {
+//    $ilm ="hea";
+//    echo "Väljas on ilus ilm, tuju on '.$ilm '!";
+//}
+//
+$vanus= rand(0,100);
+if ($vanus>=0 and $vanus<11) {
+    $staatus="laps";
+
+}
+elseif ( $vanus>10 and $vanus<18) {
+    $staatus = "nooruk";
+}
+elseif($vanus>17 and $vanus<65){
+    $staatus="täiskasvanu";
 }
 else {
-    $ilm ="hea";
-    echo "Väljas on ilus ilm, tuju on" '.$ilm !";
+    $staatus="seenior";
 }
+echo 'Oled ' .$vanus. ' aastat vana.<br/>';
+echo 'Oled ' .$staatus. '<br/>';
 
 ?>
